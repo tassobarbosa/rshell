9f5e5785208c0fae62198b865b9c0e571aedcf99
tassoeb <tasso_barbosa@yahoo.com.br>
2014-10-21 00:15:25 -0700

    validating && and || if spaced

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
 
 using namespace std;
+
+static int *glob_flag;
 
 //output login @ machine $
 void prompt(){
@@ -55,14 +58,19 @@ void comment(char line[],int linesize){
 		 memset(&line[hashtag-line],0,linesize);			
 	}
 }
+
 void execute(char *str[],int size){
-//for(int i=0;i<size;i++) cout<<str[i]<<endl;
 
 char * newstr[256];
 char * connector;
 int i,j,aux;
 
+	//create a shared memory to be accessed from child and process
+	glob_flag =(int*) mmap(NULL, sizeof *glob_flag, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
 	for(i=0;i<size;i++){
+		*glob_flag = 0;
 		//newstr[0] receives commands, first parameter after a connector
 		newstr[0] = str[i];
 		aux=1;
@@ -76,6 +84,18 @@ int i,j,aux;
 				//erase the last character if ';'
 					str[j][connector-str[j]] = '\0';
 				}
+				//check for && setting a flag to 1
+				if(memcmp(str[j], "&&",2)==0){
+					*glob_flag = 1;	
+					i = j;
+					break;				
+				}
+				//check for || setting a flag to 3
+				if(memcmp(str[j], "||",2)==0){
+					*glob_flag = 3;	
+					i = j;
+					break;				
+				}
 
 				//add flags to newstr
 				newstr[aux] = str[j];
@@ -84,29 +104,39 @@ int i,j,aux;
 	
 				if(connector!=NULL) break;
 			}
-//	for(int g=0;g<aux+2;g++)cout<<"indice "<<g<<" "<<newstr[g]<<endl;
 	
 		int pid = fork();
 		if(pid == -1){
-			perror("There was an error with fork().");
+			perror("fork() presented error");
 			exit(1);
 		}
 		else if(pid == 0){					
-		  	 if(-1 == execvp(newstr[0], newstr)){
+		  	 if(-1 == execvp(newstr[0], newstr)){	
 				perror("There was an error");
-			}		
-    	 		 exit(1);
+				//flag 1 means, first command must be successfull to apply the second
+				if(*glob_flag == 1) *glob_flag = 2;
+				//flag 3 means, first command must be failed to apply the second
+				//number 5 won't pass in the breaking loop
+				if(*glob_flag == 3) *glob_flag = 5;
+			}					
+    	 		exit(1);
 		}
 		else if(pid>0){
 			if(-1 == wait(0))
-		   	perror("There was an error with wait()");
+		   	perror("wait() presented error");
 		
 		}
 	
 		// clear the vector newstr in order to execute new commands
 		for(int k=0;k<aux;k++)newstr[k]='\0';
+
+		//break loop due to a non valid previous command connected by &&					
+		if(*glob_flag == 2) break;
+		//break loop due to a valid previous command connected by ||
+		if(*glob_flag == 3) break;
 	}
 }
+
 
 
 
@@ -118,12 +148,14 @@ int main(){
 
    while(true){
 	do{
-	    prompt(); //output login @ machine $
+	    //output login @ machine $
+	    prompt();
 	    cin.getline(input,256);
 	}while(input[0]=='#');
 
 	comment(input,INPUTSIZE);
 
+	//built in function to finish program when typed 'EXIT'
 	if(memcmp(input, "exit",4)==0) exit(0);
 
 
@@ -137,7 +169,6 @@ int main(){
 	}
 
 	str[index] = NULL;
-	//for(int i=0;i<sizeof(str);i++) cout<<str[i]<<endl;
 
 	execute(str,index);
 
